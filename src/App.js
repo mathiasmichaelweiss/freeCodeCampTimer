9ec0e48208c2666370cf86b0e2614e1324b56da5
mathiasweiss <mathiasmichaelweiss@gmail.com>
2021-08-05 16:40:14 +0200

    full app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import react from "react";
+import classes from "./App.module.css";
+import { Main } from "./components/Main/Main";
+import { connect } from "react-redux";
 
-function App() {
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.App}>
+      <Main />
     </div>
   );
 }
 
-export default App;
+function mapStateToProps(state) {
+  return state;
+}
+
+export default connect(mapStateToProps)(App);
